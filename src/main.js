0bba596e42f77e442a57fa85be51e2e263965223
lyc017 <2728500190@qq.com>
2023-01-13 16:53:27 +0800

    111

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+// 引入的不再是Vue构造函数，而是一个createApp的工厂函数
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+// 以前
+// const vm = new Vue({
+//   render: h=>h(App)
+// })
+// vm.$mount('#app')
